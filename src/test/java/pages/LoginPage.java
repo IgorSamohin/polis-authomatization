14f7fa24a38ebcb3660242600ee8bbf8a080f4d2
IgorSamohin <igor04012001@yandex.ru>
2021-10-05 20:35:10 +0300

    Add classes

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,10 @@
 package pages;
 
+import pages.base.BasePage;
+
 public class LoginPage extends BasePage {
+    public UserMainPage login(String login, String password){
+
+        return new UserMainPage();
+    }
 }
