d948e798902470a0f66c135a850b339282f3b58c
IgorSamohin <igor04012001@yandex.ru>
2021-10-05 21:13:02 +0300

    Add files after lesson

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import pages.base.BasePage;
 
 public class LoginPage extends BasePage {
-    public UserMainPage login(String login, String password){
+    private final static By LOGIN_FIELD_LOCATOR = By.xpath("//*[@data-l='t,login' and @type='text']");
+    private final static By PASSWORD_FIELD_LOCATOR = By.xpath("//*[@data-l='t,password' and @type='password']");
+    private final static By LOGIN_BUTTON_LOCATOR = By.xpath("//*[@data-l='t,sign_in' and @type='submit']");
 
+    public UserMainPage login(String login, String password){
+        //todo
         return new UserMainPage();
     }
 }
--- src/test/java/pages/ToolBar.java
+++ src/test/java/pages/ToolBar.java
@@ -2,10 +2,12 @@ package pages;
 
 import static com.codeborne.selenide.Selenide.$;
 import org.openqa.selenium.By;
+import pages.base.BasePage;
+import pages.music.MusicMainPage;
 
-public class ToolBar {
-
-    private final By GO_TO_MUSIC_MAIN_PAGE_BUTTON_LOCATOR = By.xpath("//*[@class='toolbar_nav']//*[@data-l='t,music']");
+public class ToolBar extends BasePage {
+    private final static By GO_TO_MUSIC_MAIN_PAGE_BUTTON_LOCATOR =
+            By.xpath("//*[@data-l='t,music']//*[@class='toolbar_nav_i_ic']");
 
     public MusicMainPage clickOnMusicMainPage() {
         $(GO_TO_MUSIC_MAIN_PAGE_BUTTON_LOCATOR).click();
@@ -14,6 +16,7 @@ public class ToolBar {
     }
 
     public UserMainPage clickOnUserMainPage() {
+        //todo
         return null;
     }
 }
