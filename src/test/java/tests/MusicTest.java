43f9cc47b9b4a096375c15c30939889921452d12
IgorSamohin <igor04012001@yandex.ru>
2021-10-05 19:31:39 +0300

    Init

---
+++ src/test/java/tests/MusicTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class MusicTest extends TestBase {
+
+    @Test
+    public void test(){
+
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,12 @@
+package tests;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Test;
+
+public class TestBase {
+    WebDriverRunner driver;
+    @Test
+    public void test(){
+
+    }
+}
