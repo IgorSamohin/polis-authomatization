908c5b8b1b31703dbe909778eb7c457e9d62dbc0
Solyankin Ilia <evil.enot.00@gmail.com>
2021-10-12 19:34:27 +0300

    Fix comments

--- src/test/java/pages/music/MyMusicPage.java
+++ src/test/java/pages/music/MyMusicPage.java
@@ -1,22 +1,8 @@
 package pages.music;
 
-import static com.codeborne.selenide.Selenide.actions;
-
 public class MyMusicPage extends MusicMainPage {
 
     public MusicList getMusicList() {
         return new MusicList();
-    }
-
-    public boolean isCurrentTrack(String trackData) {
-        MusicList list = new MusicList();
-        for (int i = 0; i < list.size(); i++) {
-            //Чтобы текста песен совпадали нужно чтоли и там и там был hover на них, либо его не было нигде
-            //Я решил сделать, чтобы он был и там и там
-            actions().moveToElement(list.getWebElement(i)).build().perform();
-            if (trackData.equals(list.getTrack(i).getText()))
-                return true;
-        }
-        return false;
     }
 }
