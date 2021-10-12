2e12bb5969c980a56e143f1bb0280aa6802a47da
Solyankin Ilia <evil.enot.00@gmail.com>
2021-10-12 19:34:27 +0300

    Fix comments

--- src/test/java/pages/music/MusicTrack.java
+++ src/test/java/pages/music/MusicTrack.java
@@ -1,28 +1,27 @@
 package pages.music;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.actions;
 
 public class MusicTrack {
-    private static final By ADD_MUSIC_TO_MY_MUSIC = By.xpath("//*[@name='controls']//*[@data-l='t,add']");
-    private static final By REMOVE_MUSIC_FROM_MY_MUSIC = By.xpath("//*[@name='controls']//*[@data-tsid='remove_track']");
+    private static final String ADD_MUSIC_TO_MY_MUSIC = "//*[@name='controls']//*[@data-l='t,add']";
+    private static final String REMOVE_MUSIC_FROM_MY_MUSIC = "//*[@name='controls']//*[@data-tsid='remove_track']";
 
-    private WebElement track;
+    private SelenideElement track;
 
-    public MusicTrack(WebElement track) {
+    public MusicTrack(SelenideElement track) {
         this.track = track;
     }
 
     public void addTrackToFavorite() {
         actions().moveToElement(track).build().perform();
-        track.findElement(ADD_MUSIC_TO_MY_MUSIC).click();
+        track.$(ADD_MUSIC_TO_MY_MUSIC).click();
     }
 
     public void removeTrackFromFavorite() {
         actions().moveToElement(track).build().perform();
-        track.findElement(REMOVE_MUSIC_FROM_MY_MUSIC).click();
+        track.$(REMOVE_MUSIC_FROM_MY_MUSIC).click();
     }
 
     public String getText() {
