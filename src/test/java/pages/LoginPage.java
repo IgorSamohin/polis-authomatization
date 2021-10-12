72eb34c1a4d56d2795f8e2d9326a8bd8a7f15a30
Solyankin Ilia <evil.enot.00@gmail.com>
2021-10-12 19:50:26 +0300

    Test

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -10,6 +10,7 @@ public class LoginPage extends BasePage {
     private final static String PASSWORD_FIELD_LOCATOR = "//*[@data-l='t,password' and @type='password']";
     private final static String LOGIN_BUTTON_LOCATOR = "//*[@data-l='t,sign_in' and @type='submit']";
 
+
     public UserMainPage login(String login, String password) {
         $x(LOGIN_FIELD_LOCATOR).shouldBe(visible.because(IT_MUST_BE_VISIBLE_MESSAGE)).sendKeys(login);
         $x(PASSWORD_FIELD_LOCATOR).shouldBe(visible.because(IT_MUST_BE_VISIBLE_MESSAGE)).sendKeys(password);
